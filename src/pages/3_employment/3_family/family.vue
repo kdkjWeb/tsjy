a7078757b174eb92c12cda0358daeeb71077cf9d
Mac <1270201010@qq.com>
2018-04-17 14:41:59 +0800

    就业模块

---
+++ src/pages/3_employment/3_family/family.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="family">
+      <div class="teacher" v-for="(item,index) in teacherList" :key="index">
+          <img :src="item.src" alt="">
+          <div class="family_content">
+             <h4>{{item.subject}}</h4>
+             <p>{{item.name}}</p>
+             <p>{{item.sex}}</p>
+             <p>{{item.age}}岁</p>
+             <p>{{item.empiric}}年工作经验</p>
+             <p class="family_des">{{item.content}}</p>
+             <span>{{item.time}}</span>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export {default} from './familyCtr'
+</script>
+
+<style scoped="">
+.family{
+    width: 1068px;
+    /* height: 5000px; */
+    border: 1px solid #ddd;
+    box-shadow: 2px 2px 2px 2px #ddd;
+    margin: 0 auto;
+    padding: 65px 60px 0;
+    display: flex;
+    display: -webkit-flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+}
+.teacher{
+    padding: 30px;
+    width: 450px;
+    border: 1px solid #ddd;
+    margin-bottom: 40px;
+}
+.teacher img{
+    width: 245px;
+    height: 340px;
+}
+.family_content{
+    position: relative;
+    float: right;
+    width: 180px;
+    color: #656565;
+}
+.family_content h4{
+    color: #000;
+}
+.family_content p{
+    padding-top: 15px;
+}
+.family_content p.family_des{
+    padding-top: 35px;
+    word-break: break-all;
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 5;
+    overflow: hidden;
+}
+.family_content span{
+    position: absolute;
+    right: 0;
+    bottom: -50px;
+}
+</style>
+
+
+
--- src/router/routers.js
+++ src/router/routers.js
@@ -2,6 +2,11 @@
  * Created by kdkjPC on 2018/4/14.
  */
 import index from '@/pages/index/index'
+import employment from '@/pages/3_employment/0_activeH/activeH'
+import recruitment from '@/pages/3_employment/1_recruitment/recruitment'
+import job from '@/pages/3_employment/2_job/job'
+import family from '@/pages/3_employment/3_family/family'
+
 
 
 export default {
@@ -10,6 +15,26 @@ export default {
       path:"/",
       name:index,
       component:index
+    },{
+      path: '/employment',
+      name: 'employment',
+      component: employment,
+      redirect: '/employment/recruitment',
+      children: [
+        {
+          path: '/employment/recruitment',
+          name: 'recruitment',
+          component: recruitment
+        },{
+          path: '/employment/job',
+          name: 'job',
+          component: job
+        },{
+          path: '/employment/family',
+          name: 'family',
+          component: family
+        }
+      ]
     }
   ]
 }
