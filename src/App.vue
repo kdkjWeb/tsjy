f1f39e84845dc212d6b600ddade2ff951819e0c7
sherlly <511987975@qq.com>
2018-04-17 11:00:08 +0800

    新增活动页面

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <div class="headerBox">
     <header class="header">
       <div class="header_btn">
         <div class="login">登录</div>
@@ -9,9 +10,10 @@
         <img src="@/assets/images/login.jpg" alt="">
       </div>
     </header>
+    </div>
     <nav class="nav">
       <ul class="nav_bar">
-        <li class="nav_bar_title" v-for="(item,index) in navList" :key="index">
+        <li class="nav_bar_title" :class="index==thisIndex?'nav_bar_titleA':''" v-for="(item,index) in navList" :key="index" @click="toLink(index)">
           <a v-if="index !=6 " href="javascript:void(0)">{{item.title}}</a>
           <div v-else>
             <div><a href="">{{item.title}}</a></div>
@@ -34,9 +36,10 @@
     name: 'App',
     data(){
       return {
+        thisIndex:0,
         navList: [
           {
-            title: '首页'
+            title: '首页',
           },{
             title: '快讯'
           },{
@@ -63,21 +66,62 @@
           },{
             title: '探城'
           },{
-            title: '活动'
+            title: '活动',
+            href:''
           }
         ]
+      }
+    },
+    methods:{
+      toLink(index){
+        this.thisIndex = index;
+        switch (index) {
+          case 0:
+            this.$router.push({
+              path: "/"
+            });
+            break;
+          case 1:
+            this.$router.push({
+              path: "/bSM"
+            });
+            break;
+          case 2:
+            this.$router.push({
+              path: "/scroll"
+            });
+            break;
+          case 9:
+            this.$router.push({
+              path: "/active"
+            });
+            break;
+        }
+      }
+    },
+    mounted(){
+      if (this.$route.path.indexOf("/active")>=0) {
+        this.thisIndex = 9;
       }
     }
   }
 </script>
 <!--清除样式-->
 <style>
+  html {
+    height:100%;
+    overflow: auto;
+  }
   html,body{
     width:100%;
-    height:100%;
+    position:relative;
     padding:0;
     margin:0;
     min-width:1200px;
+    background-color: #f2f2f2;
+  }
+  body {
+    min-height:100%;
   }
   * {
     padding:0;
@@ -88,6 +132,9 @@
     height:100%;
     font-size:14px;
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  }
+  ul {
+    padding:0;
   }
   li {
     list-style: none;
@@ -113,10 +160,40 @@
   .el-table .cell {
     white-space: nowrap;
   }
+  .el-pagination.is-background .btn-next, .el-pagination.is-background .btn-prev, .el-pagination.is-background .el-pager li {
+    width:35px;
+    height:35px;
+    background-color: white;
+    border:3px solid #e2e2e2;
+    line-height: 30px;
+    font-size:12px;
+    color:#999;
+  }
+  .el-pagination.is-background .btn-next:hover, .el-pagination.is-background .btn-prev:hover, .el-pagination.is-background .el-pager li:hover {
+    background-color: #97857b;
+    border-color:#97857b;
+    color:white;
+  }
+  .el-pagination.is-background .el-pager li:not(.disabled).active {
+    background-color: #97857b;
+    border-color:#97857b;
+    color:white;
+  }
+  .el-pagination.is-background .el-pager li:not(.disabled):hover {
+    color:white;
+  }
+  .width12 {
+    width:1200px;
+    margin:0 auto;
+    overflow: auto;
+  }
 
 
 /*公用头部样式*/
-
+.headerBox {
+  width:100%;
+  background-color: #fff;
+}
   .header{
     width: 1200px;
     height: 130px;
@@ -173,6 +250,9 @@
   .nav>ul.nav_bar li.nav_bar_title:hover{
     background-color: #5b565d;
   }
+  .nav>ul.nav_bar li.nav_bar_titleA {
+    background-color: #5b565d;
+  }
   .nav>ul.nav_bar li:nth-child(7):hover ul.secondNav{
     display: block;
   }
@@ -198,10 +278,15 @@
   ul.secondNav li.secondNav_bar:hover{
     background-color: #97857b;
   }
+  ul.secondNav li.secondNav_barA {
+    background-color:#97857b ;
+  }
 
 
 
   .footer{
+    position:absolute;
+    bottom:0;
     width: 100%;
     min-width: 1200px;
     height: 60px;
--- src/router/routers.js
+++ src/router/routers.js
@@ -2,14 +2,39 @@
  * Created by kdkjPC on 2018/4/14.
  */
 import index from '@/pages/index/index'
+//活动
+import activeH from '@/pages/10_active/0_activeH/activeH'
+import affiche from '@/pages/10_active/1_affiche/affiche'
+import luckDraw from '@/pages/10_active/2_luckDraw/luckDraw'
+import vote from '@/pages/10_active/3_vote/vote'
 
 
 export default {
   routes: [
     {
       path:"/",
-      name:index,
+      name:"name",
       component:index
+    }, {
+      path:"/active",
+      name:"activeH",
+      component:activeH,
+      redirect:'/active/affiche',
+      children:[{
+        path:'/active/affiche',
+        name:"affiche",
+        component:affiche
+      },{
+        path:'/active/luckDraw',
+        name:"luckDraw",
+        component:luckDraw
+      },{
+        path:'/active/vote',
+        name:"vote",
+        component:vote
+      },
+
+      ]
     }
   ]
 }
