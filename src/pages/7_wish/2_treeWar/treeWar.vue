61a4e26043c73fb21255720199c04087be33da04
sherlly <511987975@qq.com>
2018-04-18 17:31:32 +0800

    no message

--- src/pages/7_wish/2_treeWar/treeWar.vue
+++ src/pages/7_wish/2_treeWar/treeWar.vue
@@ -1,4 +1,5 @@
 <template>
+  <div>
     <div class="container">
       <div class="ad">
         <img src="../../../assets/images/treeWar.png" alt="">
@@ -8,20 +9,55 @@
         </div>
       </div>
       <div class="list">
-        <div class="listB"></div>
+        <div class="listB clear" v-for="item,index in list" :key="index">
+          <div class="headImg">
+            <img :src="item.src" alt="">
+            <p>{{item.name}}</p>
+          </div>
+          <div class="listC">
+            <p>{{item.title}}</p>
+            <p><span>{{item.timer}}</span><i class="iconfont icon-icon_share"></i></p>
+          </div>
+        </div>
       </div>
+      <el-pagination class="pagination"
+                     background
+                     @current-change="handleCurrentChange"
+                     :current-page.sync="currentPage1"
+                     :page-size="100"
+                     :total="1000"
+                     layout="prev, pager, next">
+      </el-pagination>
+
     </div>
+    <div class="send clear">
+      <div>
+        <div class="sendImg">
+          <img src="../../../assets/wish1.png" alt="">
+        </div>
+        <div class="text">
+          <textarea placeholder="想说什么就说什么吧"></textarea>
+        </div>
+      </div>
+      <button class="sendSecret"><i class="iconfont icon-fasong"></i>发送</button>
+    </div>
+  </div>
+
 </template>
 <script>
     export default require('./treeWarCtr');
 </script>
 <style scoped>
+  .pagination {
+    text-align: center;
+    margin-top:50px;
+  }
   .container {
     width:1200px;
     padding:50px;
     box-sizing: border-box;
     background-color: white;
-    margin:0 auto 100px;
+    margin:0 auto;
   }
 
   .ad {
@@ -56,5 +92,84 @@
   }
   .listB:last-child{
     border-bottom:none;
+  }
+  .headImg {
+    width:200px;
+    text-align: center;
+    float: left;
+  }
+  .headImg img {
+    width:70px;
+    height:70px;
+    border-radius: 100%;
+  }
+  .headImg p {
+    margin-top:10px;
+  }
+  .listC {
+    width:860px;
+    float: left;
+  }
+  .listC p:nth-of-type(1) {
+    width:100%;
+    /*height:80px;*/
+    /*overflow: auto;*/
+  }
+  .listC p:nth-of-type(2) {
+    margin-top:70px;
+    color:#999;
+  }
+  .listC p i{
+    float: right;
+    cursor: pointer;
+  }
+
+  .send {
+    width:1200px;
+    background-color: white;
+    margin:50px auto 100px;
+    box-sizing: border-box;
+    padding:30px;
+  }
+  .sendImg {
+    width:70px;
+    margin-right:15px;
+  }
+  .sendImg img {
+    width:70px;
+    height:70px;
+    border-radius: 100%;
+  }
+  .send>div {
+    width:100%;
+    display: flex;
+    flex-direction: row;
+  }
+  .send .text {
+    flex: 1;
+  }
+  .send textarea {
+    width:100%;
+    border:1px solid #e2e2e2;
+    resize:none;
+    border-radius: 40px;
+    height:100px;
+    padding:15px 30px;
+    box-sizing: border-box;
+  }
+  .sendSecret {
+    width:100px;
+    height:40px;
+    background-color: #97857b;
+    border:none;
+    outline: none;
+    border-radius: 40px;
+    float: right;
+    margin-top:15px;
+    color:white;
+    cursor: pointer;
+  }
+  .sendSecret i {
+    margin-right:10px;
   }
 </style>
