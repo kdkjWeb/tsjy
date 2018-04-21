4e52fa9bbeb66fa90e0f22473121eb5c00d84aa6
sherlly <511987975@qq.com>
2018-04-21 18:33:27 +0800

    no message

---
+++ src/pages/3_employment/1_1_jobDetail/jobDetailCtr.js
@@ -0,0 +1,19 @@
+/**
+ * Created by kdkjPC on 2018/4/21.
+ */
+export default {
+  data(){
+    return {
+      showPhone:false,
+      list:["五险一金","年底三薪","包吃","包住","旅游"]
+    }
+  },
+  methods:{
+    seePhone(){
+      this.showPhone = true;
+    }
+  },
+  mounted(){
+
+  }
+}
