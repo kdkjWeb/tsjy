52a535223e1a86b890fa023f0e0baa9a56f8e8f3
sherlly <511987975@qq.com>
2018-04-23 11:29:30 +0800

    no message

--- src/pages/3_employment/3_2_familyForm/familyForm.vue
+++ src/pages/3_employment/3_2_familyForm/familyForm.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="container">
-    <div class="headerT">基本信息</div>
     <div class="conB">
       <el-form ref="form" :model="form" label-width="90px">
+        <el-form-item label="个人照片 ：">
+          <div class="el-upload el-upload--picture-card input">
+            <i class="el-icon-plus"></i>
+            <img :src="src" alt="">
+            <input type="file" class="inputI"  @change="upPic($event)">
+          </div>
+        </el-form-item>
         <el-form-item label="姓名 ：">
           <el-input v-model="form.name" placeholder="请输入姓名"></el-input>
         </el-form-item>
-        <el-form-item label="工作意向 ：">
-          <el-input v-model="form.nickname" placeholder="请输入职位"></el-input>
-        </el-form-item>
-        <el-form-item label="期望薪资 ：">
-          <el-input v-model="form.height" type="number" placeholder="请输入期望薪资"></el-input>
+        <el-form-item label="家教专业 ：">
+          <el-input v-model="form.nickname" placeholder="请输入家教"></el-input>
         </el-form-item>
         <el-form-item label="性别 ：">
           <el-select v-model="form.sex" placeholder="请选择性别">
@@ -21,48 +24,26 @@
         <el-form-item label="年龄 ：">
           <el-input v-model="form.year" type="number" placeholder="请输入年龄"></el-input>
         </el-form-item>
-
-        <el-form-item label="现居地址 ：">
-          <el-input v-model="form.weight" placeholder="请输入地址"></el-input>
-        </el-form-item>
-        <el-form-item label="学校 ：">
-          <el-input v-model="form.school" placeholder="请输入学校"></el-input>
-        </el-form-item>
         <el-form-item label="学历 ：">
-          <el-input v-model="form.country" placeholder="请输入学历"></el-input>
+          <el-input v-model="form.height" placeholder="请输入学历"></el-input>
         </el-form-item>
-        <el-form-item label="年限 ：">
-          <el-input v-model="form.nation" type="number" placeholder="请输入年限"></el-input>
+        <el-form-item label="毕业学校 ：">
+          <el-input v-model="form.weight" placeholder="请输入毕业学校"></el-input>
         </el-form-item>
-        <el-form-item label="自我介绍 ：">
-          <el-input type="textarea" :rows="5" v-model="form.nation"></el-input>
+        <el-form-item label="工作经验 ：">
+          <el-input v-model="form.school" placeholder="请输入工作经验"></el-input>
         </el-form-item>
-
-      </el-form>
-    </div>
-    <div class="headerT">工作经历</div>
-    <div class="conB">
-      <el-form ref="form" :model="form" label-width="90px">
-        <el-form-item label="活动时间">
-          <el-col :span="11">
-            <el-date-picker type="date" placeholder="选择日期" v-model="form.date1" style="width: 100%;"></el-date-picker>
-          </el-col>
-          <el-col class="line" :span="2" style="text-align: center">-</el-col>
-          <el-col :span="11">
-            <el-date-picker type="date" placeholder="选择日期" v-model="form.date2" style="width: 100%;"></el-date-picker>
-          </el-col>
+        <el-form-item label="联系手机 ：">
+          <el-input v-model="form.country" type="number" placeholder="请输入手机"></el-input>
         </el-form-item>
-        <el-form-item label="工作职位 ：">
-          <el-input v-model="form.nickname" placeholder="请输入职位"></el-input>
-        </el-form-item>
-        <el-form-item label="薪资待遇 ：">
-          <el-input v-model="form.height" type="number" placeholder="请输入期望薪资"></el-input>
+        <el-form-item label="擅长学科 ：">
+          <el-input v-model="form.nation" placeholder="请输入擅长学科"></el-input>
         </el-form-item>
         <el-form-item label="自我介绍 ：">
           <el-input type="textarea" :rows="5" v-model="form.nation"></el-input>
         </el-form-item>
       </el-form>
-      <div class="submitBtn">确定</div>
+      <div class="submitBtn">报名</div>
     </div>
   </div>
 </template>
@@ -73,20 +54,13 @@
   .container {
     width:1200px;
     padding:50px;
+    background-color: white;
     box-sizing: border-box;
     margin:50px auto 100px;
-    background-color: white;
   }
-  .headerT {
-    width:100%;
-    padding:10px 0 10px 20px;
-    text-align: center;
-    background-color: #f2f2f2;
-  }
-
   .conB {
     width:460px;
-    margin:40px auto;
+    margin:0 auto;
   }
   .el-select {
     width:100%;
@@ -99,7 +73,7 @@
     color:white;
     font-size:16px;
     letter-spacing: 2px;
-    background-color: #4d484f;
+    background-color: #da854c;
     margin:50px auto;
   }
   .input {
@@ -125,4 +99,5 @@
     width:150px;
     height:150px;
   }
+
 </style>
